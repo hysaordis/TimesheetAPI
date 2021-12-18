b50f88f5f55576d7cd1754c0cf2fb978b3e35219
ordishysa <ordishysa@gmail.com>
2021-12-18 17:09:58 +0100

    All timesheet required

--- TimesheetAPI/Repositories/DBModels/Timesheet.cs
+++ TimesheetAPI/Repositories/DBModels/Timesheet.cs
@@ -1,18 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using TimesheetAPI.Model.DbModels;
 
 namespace TimesheetAPI.Repositories.DBModels
 {
     public class Timesheet
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public int ProjectId { get; set; }
+
+        [Required]
         public Project Project { get; set; }
+
+        [Required]
         public int ActivityTypeId { get; set; }
+
+        [Required]
         public ActivityType ActivityType { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
+
+        [Required]
         public int Hours { get; set; }
+
+        [Required]
         public ApplicationUser Employees { get; set; }
     }
 }
